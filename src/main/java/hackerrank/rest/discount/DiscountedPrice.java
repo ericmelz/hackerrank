95792cd0978d10c9b7e94bfe1504ebb86ad94082
Eric Melz <emelz@gumgum.com>
2023-07-12 13:56:43 -0700

    remove use of HttpClient

--- src/main/java/hackerrank/rest/discount/DiscountedPrice.java
+++ src/main/java/hackerrank/rest/discount/DiscountedPrice.java
@@ -1,19 +1,15 @@
 package hackerrank.rest.discount;
 
-import java.io.*;
-import java.math.*;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.security.*;
-import java.text.*;
-import java.time.Duration;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-import java.net.*;
-import org.json.simple.*;
-import org.json.simple.parser.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DiscountedPrice {
     /*
@@ -24,34 +20,45 @@ public class DiscountedPrice {
      * API URL: https://jsonmock.hackerrank.com/api/inventory?barcode=<barcode>
      */
     public static final String BARCODE_URI = "https://jsonmock.hackerrank.com/api/inventory?barcode=";
+    private static final String USER_AGENT = "Mozilla/5.0";
 
     public static int getDiscountedPrice(int barcode) {
-        HttpClient client = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_1_1)
-                .followRedirects((HttpClient.Redirect.NORMAL))
-                .connectTimeout(Duration.ofSeconds(20))
-                .build();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(BARCODE_URI + barcode))
-                .build();
-
-        HttpResponse<String> response = null;
+        URL obj = null;
+        HttpURLConnection con = null;
         try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            Object obj = JSONValue.parse(response.body());
-            JSONObject jsonObject = (JSONObject) obj;
+            obj = new URL(BARCODE_URI + barcode);
+            con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+            int responseCode = 0;
+            responseCode = con.getResponseCode();
+            System.out.println("GET Response Code :: " + responseCode);
+            StringBuffer response = new StringBuffer();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+
+                while (true) {
+                    if (!((inputLine = in.readLine()) != null)) break;
+                    response.append(inputLine);
+                }
+                in.close();
+            } else {
+                System.out.println("GET request did not work.");
+            }
+
+            Object object = JSONValue.parse(response.toString());
+            JSONObject jsonObject = (JSONObject) object;
             JSONArray jsonArray = (JSONArray) jsonObject.get("data");
             if (jsonArray.size() == 0) {
                 return -1;
             }
             jsonObject = (JSONObject) jsonArray.get(0);
-            long price = (long)jsonObject.get("price");
-            long discount = (long)jsonObject.get("discount");
-            long discountedPrice = (long)(price - ((discount / 100.0) * price));
-            return (int)discountedPrice;
+            long price = (long) jsonObject.get("price");
+            long discount = (long) jsonObject.get("discount");
+            long discountedPrice = (long) (price - ((discount / 100.0) * price));
+            return (int) discountedPrice;
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
